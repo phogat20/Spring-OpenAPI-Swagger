41c2ce16cc5d0fdeae7129b3b2389fd1db6d0609
CHETAN SINGH PHOGAT <84695818+phogat20@users.noreply.github.com>
2023-04-23 14:03:13 +0530

    Create OpenAPI3.java

---
+++ OpenAPI3.java
@@ -0,0 +1,31 @@
+# If you want to use the newer OpenAPI 3 specification instead of the older Swagger 2 specification, you can use the following dependencies instead:
+
+// <dependency>
+//     <groupId>io.springfox</groupId>
+//     <artifactId>springfox-boot-starter</artifactId>
+//     <version>3.0.0</version>
+// </dependency>
+
+// - This dependency includes both the OpenAPI 3 specification support and the Swagger UI.
+
+@Configuration
+@EnableOpenApi
+public class OpenApiConfig {
+    @Bean
+    public GroupedOpenApi customOpenApi() {
+        String[] paths = { "/api/**" };
+        return GroupedOpenApi.builder()
+                .group("custom-api")
+                .pathsToMatch(paths)
+                .build();
+    }
+    
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Custom API")
+                        .description("This is a custom API.")
+                        .version("1.0.0"));
+    }
+}
